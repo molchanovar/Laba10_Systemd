1769ce77b088aa4301ab44158cba50481a83817b
molchanovar <64701071+molchanovar@users.noreply.github.com>
2021-02-27 17:26:43 +0300

    Update provision2.sh

--- task2/provision2.sh
+++ task2/provision2.sh
@@ -2,8 +2,7 @@
 yum install epel-release -y && yum install spawn-fcgi php php-cli mod_fcgid httpd -y
 
 # Раскомментирование конфиг-файла /etc/sysconfig/spawn-fcgi
-sed -i 's/#SOCKET/SOCKET/' /etc/sysconfig/spawn-fcgi
-sed -i 's/#OPTIONS/OPTIONS/' /etc/sysconfig/spawn-fcgi
+sed -i -e 's/#OPTIONS/OPTIONS/; s/#SOCKET/SOCKET/' /etc/sysconfig/spawn-fcgi
 
 # Добавление юнита 
 cp /vagrant/task2/spawn-fcgi.service /etc/systemd/system/spawn-fcgi.service
